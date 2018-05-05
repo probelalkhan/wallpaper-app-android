42c7e3fa25f464661e1c75c89c4996efe7eab0b5
Belal Khan <probelalkhan@gmail.com>
2018-05-05 19:50:16 +0530

    Settings Fragment

--- app/src/main/java/net/simplifiedcoding/wallpaperhub/fragments/SettingsFragment.java
+++ app/src/main/java/net/simplifiedcoding/wallpaperhub/fragments/SettingsFragment.java
@@ -8,8 +8,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -20,6 +23,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 import net.simplifiedcoding.wallpaperhub.R;
@@ -36,22 +40,53 @@ public class SettingsFragment extends Fragment {
         if (FirebaseAuth.getInstance().getCurrentUser() == null) {
             return inflater.inflate(R.layout.fragment_settings_default, container, false);
         }
-        return inflater.inflate(R.layout.fragment_settings_default, container, false);
+        return inflater.inflate(R.layout.fragment_settings_logged_in, container, false);
     }
 
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         GoogleSignInOptions gso =
                 new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                         .requestIdToken(getString(R.string.default_web_client_id))
                         .build();
-
         mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
 
         if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+
+            ImageView imageView = view.findViewById(R.id.image_view);
+            TextView textViewName = view.findViewById(R.id.text_view_name);
+            TextView textViewEmail = view.findViewById(R.id.text_view_email);
+
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+            Glide.with(getActivity())
+                    .load(user.getPhotoUrl().toString())
+                    .into(imageView);
+
+
+            textViewName.setText(user.getDisplayName());
+            textViewEmail.setText(user.getEmail());
+
+
+            view.findViewById(R.id.text_view_logout).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    FirebaseAuth.getInstance().signOut();
+                    mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            getActivity().getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.content_area, new SettingsFragment())
+                                    .commit();
+                        }
+                    });
+
+
+                }
+            });
 
         } else {
 
@@ -91,7 +126,11 @@ public class SettingsFragment extends Fragment {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(getActivity(), "Login Successful", Toast.LENGTH_LONG).show();
+
+                            getActivity().getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.content_area, new SettingsFragment())
+                                    .commit();
+
                         } else {
                             Toast.makeText(getActivity(), "Login Failure", Toast.LENGTH_LONG).show();
                         }
